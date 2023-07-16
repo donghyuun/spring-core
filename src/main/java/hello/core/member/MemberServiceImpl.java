b0539496a30e94a9d4457a5ae980edc51b91cfe3
donghyuun <bomul0524@gmail.com>
2023-07-16 22:41:53 +0900

    회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 //구현 클래스(구현)
 public class MemberServiceImpl implements MemberService{
 
+    //인터페이스(추상화) 의존                            &        구현체(구체화) 의존   => 둘 다 의존 => 되게 안좋음!(DIP 위반)
     private final MemberRepository memberRepository = new MemoryMemberRepository();//다형성 적용
 
     @Override
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);//join한것과 찾은게 같은지 검사, 같으면 정상동작
+    }
+}
