1338922f3d174b422e9687411d4525ea322255a6
donghyuun <bomul0524@gmail.com>
2023-07-21 20:13:06 +0900

    @Configuration 과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,21 +16,35 @@ import org.springframework.context.annotation.Configuration;
 //공연 기힉자의 역할, 구체 클래스를 선택한다. 배역에 맞는 담당 배우를 선택. 애플리케이션 동작의 전체 구성을 책임진다.
 @Configuration
 public class AppConfig {
+    /*싱글톤이 아닐때 아래 메서드들을 한번씩 호출할때*/
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    /*싱글톤일때 ...*/
+    //call AppConfig.memberService
+    //calL AppConfig.memberRepository
+    //call AppConfig.orderService
 
     //생성자 주입 방법
     @Bean //스프링 컨테이너에 메서드이름을 key, 리턴값을 value 로 하여 등록됨
     public MemberService memberService(){//인터페이스(역할)
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());//구현클래스(구현)
     }
 
     @Bean
-    public static MemberRepository memberRepository() {//인터페이스(역할)
+    public MemberRepository memberRepository() {//인터페이스(역할)
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();//구현클래스(구현)
     }
 
     //생성자 주입 방법
     @Bean
     public OrderService orderService(){//인터페이스(역할)
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());//구현클래스(구현)
     }
 
