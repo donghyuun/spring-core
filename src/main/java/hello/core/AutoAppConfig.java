0b2e71fecea278f2c5b2b50279197632caa6af31
donghyuun <bomul0524@gmail.com>
2023-07-22 19:15:27 +0900

    탐색 위치와 기본 스캔 대상, 기타) CoreApplication.java 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(//@Component가 붙은 클래스들을 찾아서 스프링 빈으로 등록해줌
+        basePackages = "hello.core.member",//컴포넌트 탐색 위치 지정, 이 부분부터 하위로 컴포넌트 스캔함
+        basePackageClasses = AutoAppConfig.class,//해당 클래스가 속한 패키지부터 하위로 컴포넌트 스캔
+        //=> 지정하지 않으면 @ComponentScan 이 속한 패키지부터 하위로 컴포넌트 스캔, CoreApplication의 @SpingBootApplication 안에 이미 @ComponentScan 있음 => 내가 따로 붙힐 필요가 없다.
+
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)//@Configuration(@Component 가짐)은 이미 수동으로 추가된 빈들을 가지므로
 )
 public class AutoAppConfig {
