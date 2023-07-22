229d36e28539fbed0cb09714e93c4b9191349280
donghyuun <bomul0524@gmail.com>
2023-07-22 20:54:21 +0900

    의존 관계 주입 - 생성자 주입(지금까지 했던 것)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
     //추상(인터페이스)에만 의존 => DIP준수!!!
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;//final 은 값이 무조건 있어야 한고, 한번 초기화되면 변경할 수 없는 상수값이 된다.
     private final DiscountPolicy discountPolicy;
 
     //객체 의존관계가 주입됨 => DI(dependency injection)
-    @Autowired //자동으로 스프링 컨테이너에서 해당 타입 객체 찾아서 (의존관계)주입, ac.getBean(MemberRepository.class), ac.getBean(DiscountPolicy) 와 유사
+    @Autowired //자동으로 스프링 컨테이너에서 해당 타입 객체 찾아서 (의존관계)주입, ac.getBean(MemberRepository.class), ac.getBean(DiscountPolicy) 와 유사, 생성자 하나면 생략가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
@@ -35,3 +35,4 @@ public class OrderServiceImpl implements OrderService{
         return memberRepository;
     }
 }
+//스프링 빈 등록(이때 생성자도 호출됨) -> 의존 관계 주입 순서이므로, 함수 호출 순서는: OrderServiceImpl 함수 -> setMemberRepository 함수 & setDiscountPolicy 함수 순으로 호출됨
