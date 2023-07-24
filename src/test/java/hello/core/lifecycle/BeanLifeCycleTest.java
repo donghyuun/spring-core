8c0cca7c4341a030ea3139839dc8475ae012de25
donghyuun <bomul0524@gmail.com>
2023-07-24 20:34:58 +0900

    (단순글) 스프링 빈 이벤틀 라이브 사이클, 객체의 생성과 초기화를 분리하자

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,9 +18,11 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig{
-
+        //스프링 빈의 이벤트 라이프 사이클
+        //스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
         @Bean
         public NetworkClient networkClient() {
+            //객체의 생성과 초기화를 분리하자
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
